66a0ff770e5fa29758875332c93515c8b5d5c990
HannaMois <hannamois135@gmail.com>
2023-02-09 19:04:21 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { ThemeProvider } from '@emotion/react';
+import App from 'components/App';
 import './index.css';
+
+const theme = {
+  colors: {
+    mainColor: 'rgb(46, 34, 2)',
+    whiteColor: '#fff',
+    redColor: 'red',
+    buttonGood: 'rgb(54, 219, 48)',
+    buttonNeutral: 'rgb(169, 189, 168)',
+    buttonBad: 'rgb(207, 46, 37)',
+    bgColor: 'rgb(240, 184, 180)',
+  },
+  fontSizes: {
+    main: '40px',
+    second: '20px',
+    hover: '25px',
+  },
+  lineHeights: {
+    title: '1.2',
+  },
+  space: {
+    m: '20px',
+    xs: '0',
+  },
+  fontWeights: {
+    b: '700',
+  },
+  radii: {
+    radius: '5px',
+  },
+  sizes: {
+    w: '150px',
+    h: '50px',
+  },
+  borderWidths: {
+    w: '1px',
+  },
+};
+
+console.log(theme);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
