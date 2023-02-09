92067081283a9828d7e2102fa187887cc1ed7b62
HannaMois <hannamois135@gmail.com>
2023-02-09 18:59:49 +0200

    Create app

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Box = styled.main`
+  padding: ${props => props.theme.space.m};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
