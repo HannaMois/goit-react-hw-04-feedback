f529665561d09fc7e80fef1d6a72f014c5245b42
HannaMois <hannamois135@gmail.com>
2023-02-18 00:05:34 +0200

    Add changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Box } from './App.styled';
 import Section from '../Section';
 import Statistics from '../Statistics';
@@ -9,48 +9,14 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [positiveFeedBack, setPositiveFeedback] = useState(0);
-
-  const countTotalFeedback = () => setTotal(good + neutral + bad);
 
   const countPositiveFeedbackPercentage = () =>
-    setPositiveFeedback(Number(((good / total) * 100).toFixed(0)));
-
-  useEffect(() => {
-    countTotalFeedback();
-    countPositiveFeedbackPercentage();
-  });
-
-  // const onLeaveFeedback = option => {
-  //   switch (option) {
-  //     case 'good':
-  //       setGood(prevState => prevState + 1);
-  //       return;
-  //     case 'neutral':
-  //       setNeutral(prevState => prevState + 1);
-  //       return;
-  //     case 'bad':
-  //       setBad(prevState => prevState + 1);
-  //       return;
-  //     default:
-  //       return;
-  //   }
-  // };
+    Number(((good / total) * 100).toFixed(0));
 
   const onLeaveFeedback = option => {
-    if (option === 'good') {
-      setGood(prevState => prevState + 1);
-      return;
-    }
-    if (option === 'neutral') {
-      setNeutral(prevState => prevState + 1);
-      return;
-    }
-    if (option === 'bad') {
-      setBad(prevState => prevState + 1);
-      return;
-    }
+    if (option === 'good') setGood(p => p + 1);
+    if (option === 'neutral') setNeutral(p => p + 1);
+    if (option === 'bad') setBad(p => p + 1);
   };
 
   const options = {
@@ -58,6 +24,9 @@ const App = () => {
     neutral,
     bad,
   };
+
+  const total = good + neutral + bad;
+  const positiveFeedBack = countPositiveFeedbackPercentage();
 
   return (
     <Box>
